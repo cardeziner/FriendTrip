e591b04f2936f39ba6fad2c6b1cf83951680ef3d
Peter Stevens <cardeziner@gmail.com>
2020-09-11 15:23:33 -0600

    debugged route issues

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "/trips/:id/events", to: 'static_pages#index'
   get "/trips/:trip_id/events", to: 'static_pages#index'
   get "/trips/:trip_id/events/:id", to: 'static_pages#index'
+
   get "/aboutus", to: 'static_pages#index'
 
   namespace :api do
