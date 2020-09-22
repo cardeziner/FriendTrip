a9dd128632dd828e92c0d5931aeb9db645d8fd51
Peter Stevens <cardeziner@gmail.com>
2020-09-21 22:18:45 -0600

    about us + bootstrap

--- app/javascript/react/components/AboutUsPage.js
+++ app/javascript/react/components/AboutUsPage.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const AboutUsPage = props =>{
   return(
-    <div>
+    <div className="bg-gray row">
+      <div className="bigbox col-6">
       Welcome to about us page!
-    
+      </div>
     </div>
   )
 }
