b2c864c58b5ec8e9dc9b1e5e980bea3cd3df80ef
Peter Stevens <cardeziner@gmail.com>
2020-09-22 23:51:53 -0600

    fonts

--- app/javascript/react/components/AboutUsPage.js
+++ app/javascript/react/components/AboutUsPage.js
@@ -4,7 +4,7 @@ const AboutUsPage = props =>{
   return(
     <div className="bg-gray row">
       <h1 className="center title red">About Us</h1>
-      <div className="bigbox col-6">
+      <div className="bigbox col-6 bodytext">
       Welcome to about us page!
       </div>
     </div>
