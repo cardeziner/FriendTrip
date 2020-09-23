74c5a50e2c4ce67f689e49fa46485f537db81d30
Peter Stevens <cardeziner@gmail.com>
2020-09-22 23:32:17 -0600

    body margins  + about us

--- app/javascript/react/components/AboutUsPage.js
+++ app/javascript/react/components/AboutUsPage.js
@@ -3,6 +3,7 @@ import React from 'react'
 const AboutUsPage = props =>{
   return(
     <div className="bg-gray row">
+      <h1 className="center title red">About Us</h1>
       <div className="bigbox col-6">
       Welcome to about us page!
       </div>
