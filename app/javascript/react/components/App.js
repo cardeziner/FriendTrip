4ef35ecf746c104d5c634a8302509d0a31c490a0
Peter Stevens <cardeziner@gmail.com>
2020-09-23 20:24:54 -0600

    solving bootstrap issues

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import TripsIndexComponent from './TripsIndexComponent'
 import TripShowContainer from './TripShowContainer'
