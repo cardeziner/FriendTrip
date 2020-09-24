5b56b89ef27262800c048d20a3d10940cb8d5c06
Peter Stevens <cardeziner@gmail.com>
2020-09-23 19:51:15 -0600

    switching branches

--- app/javascript/react/components/AboutUsPage.js
+++ app/javascript/react/components/AboutUsPage.js
@@ -4,8 +4,8 @@ const AboutUsPage = props =>{
   return(
     <div className="bg-gray row">
       <h1 className="center title red">About Us</h1>
-      <div className="bigbox col-6 bodytext">
-      Welcome to about us page!
+      <div className="bigbox col-4 bodytext">
+        <h2 className="title red">Welcome to about us page!</h2>
       </div>
     </div>
   )
