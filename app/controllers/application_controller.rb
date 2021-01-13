f57631dc43080b97d5eccfd782b4236c036b25e7
Peter Stevens <cardeziner@gmail.com>
2021-01-12 23:04:38 -0700

    host config for invites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:email])
   end
 
 end
