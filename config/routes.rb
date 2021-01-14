a3d3a147394f7c55932c3e3e15269ccefa26e0bb
Peter Stevens <cardeziner@gmail.com>
2021-01-13 22:52:34 -0700

    debugging devise issues

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "/trips/:id/events", to: 'static_pages#index'
   get "/trips/:trip_id/events", to: 'static_pages#index'
   get "/trips/:trip_id/events/:id", to: 'static_pages#index'
-  get "/users/invitation/new", to: 'users/invitations#new'
+  get "/users/invitation/new(.:format)", to: 'users/invitations#new'
 
   get "/aboutus", to: 'static_pages#index'
 
@@ -25,5 +25,8 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :new, :create]
     end
   end
+
+    resources :users, only: [:index, :new, :create]
+
 
 end
