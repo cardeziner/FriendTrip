1328fc9e4d8940ab3cf3902c63e374f0c418c0a5
Peter Stevens <cardeziner@gmail.com>
2021-01-27 19:56:04 -0700

    config invite

--- config/initializers/invitation.rb
+++ config/initializers/invitation.rb
@@ -1,8 +1,8 @@
 Invitation.configure do |config|
-  config.routes = false 
+  config.routes = false
   config.user_model = '::User'
   config.user_registration_url = ->(params) { Rails.application.routes.url_helpers.sign_up_url(params) }
   # config.mailer_sender = 'reply@example.com'
-  config.routes = true
+  config.routes = false
   # config.case_sensitive_email = true
 end
