ad31a19338e481f7f424a44e78f394989c4229fe
Peter Stevens <cardeziner@gmail.com>
2021-01-29 23:45:57 -0700

    invites controller

--- app/controllers/api/v1/invites_controller.rb
+++ app/controllers/api/v1/invites_controller.rb
@@ -4,4 +4,22 @@ class Api::V1::InvitesController < ApplicationController
     invite = Invite.new
   end
 
+  def create
+    invite = Invite.new(invite_params)
+
+    if @order.save
+      flash[:success] = t('flash.order.success')
+      redirect_to root_path
+    else
+      flash.now[:error] = t('flash.order.error_html')
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:email, :token)
+  end
+
 end
---
+++ app/models/invite.rb
@@ -0,0 +1,7 @@
+class Invite < ApplicationRecord
+  belongs_to :user
+  belongs_to :trip
+
+  validates :email, :token, presence: true, uniqueness: true
+
+end
