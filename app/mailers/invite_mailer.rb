5ee865574ee446e9592799e7fe59b79508063fff
Peter Stevens <cardeziner@gmail.com>
2021-02-02 17:52:48 -0700

    invite mailer errors

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,7 +3,6 @@ class InviteMailer < ApplicationMailer
   def new_invite_email
   @invite = params[:invite]
 
-  mail(to: <ADMIN_EMAIL>, subject: "You got a new order!")
-end
-
+  mail(to: <ADMIN_EMAIL>, subject: "You got a new invite!")
+  end
 end
