f46a9767e03643db54e17d0cee40c9fc7f6faefd
Peter Stevens <cardeziner@gmail.com>
2021-02-03 20:47:37 -0700

    invites controller setup

--- app/controllers/api/v1/invites_controller.rb
+++ app/controllers/api/v1/invites_controller.rb
@@ -5,9 +5,13 @@ class Api::V1::InvitesController < ApplicationController
   end
 
   def create
-    invite = Invite.new(invite_params)
+    @invite = Invite.new(invite_params)
+    @user = User.new(user_params)
+    @tripmember = TripMember.new(user_id: @invite.id, trip: trip_params)
+    
+    if @invite.save && @user.save && @tripmember.save
+      InviteMailer.with(invite: invite).new_invite_email.deliver_later
 
-    if @invite.save
       flash[:success] = t('flash.order.success')
       redirect_to root_path
     else
@@ -18,8 +22,16 @@ class Api::V1::InvitesController < ApplicationController
 
   private
 
-  def order_params
-    params.require(:order).permit(:email, :token)
+  def invite_params
+    params.require(:invite).permit(:email)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name)
+  end
+
+  def trip_params
+    params.require(:trip).permit(:trip_id)
   end
 
 end
--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,6 +3,9 @@ class InviteMailer < ApplicationMailer
   def new_invite_email
   @invite = params[:invite]
 
+  
+
   mail(to: "cardeziner@gmail.com", subject: "You got a new invite!")
   end
+
 end
