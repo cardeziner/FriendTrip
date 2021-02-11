1cc7eb19be0c713c84559223070082c89b2c4701
Peter Stevens <cardeziner@gmail.com>
2021-02-10 20:17:25 -0700

    commented out changes in progress

--- app/controllers/api/v1/invites_controller.rb
+++ app/controllers/api/v1/invites_controller.rb
@@ -4,16 +4,17 @@ class Api::V1::InvitesController < ApplicationController
     invite = Invite.new
   end
 
-  def create
 
+  def create
+    @trip = Trip.find(params[:id])
     params = user_params
-    params["password"] = "password"
+    params["password"] = password
     params["password_confirmation"] = "password"
     @user = User.new(params)
-    @tripmember = Tripmember.new(user_id: @user.id, trip_id: tripmember_params)
+    @tripmember = Tripmember.new(user_id: @user.id, trip_id: @trip.id)
     # @invite = Invite.new(email: @user.email,)
 
-    if @user.save
+    if @user.save && @tripmember.save
       InviteMailer.with(email: @user.email).new_invite_email.deliver_later
 
       render json: { user: @user }
@@ -33,8 +34,8 @@ class Api::V1::InvitesController < ApplicationController
     params.require(:user).permit(:email, :first_name, :last_name)
   end
 
-  def tripmember_params
-    params.require(:user).permit(:trip_id)
-  end
-  
+  # def tripmember_params
+  #   params.require(:user).permit(:trip_id)
+  # end
+
 end
