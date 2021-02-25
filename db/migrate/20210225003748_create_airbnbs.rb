36d631668afca8bfd3ebcb7484d3c54c96dfce19
Peter Stevens <cardeziner@gmail.com>
2021-02-24 17:38:41 -0700

    updated schema

---
+++ db/migrate/20210225003748_create_airbnbs.rb
@@ -0,0 +1,6 @@
+class CreateAirbnbs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :airbnbs do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_045813) do
+ActiveRecord::Schema.define(version: 2021_02_25_003748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airbnbs", force: :cascade do |t|
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name", null: false
