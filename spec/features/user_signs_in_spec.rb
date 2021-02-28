c5b14ede0c29b47f800d4f3387131e9727cef09a
Peter Stevens <cardeziner@gmail.com>
2021-02-27 18:59:48 -0700

    reset migrations to debug rspec issue

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,15 +6,18 @@ feature 'user signs in', %Q{
   So that I can regain access to my account
 } do
   scenario 'specify valid credentials' do
-    user = FactoryBot.create(:user)
 
+
+    user = FactoryBot.create(:user)
+    user.first_name = 'john'
+    user.last_name = 'doe'
+    user.save
     visit new_user_session_path
 
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
 
     click_button 'Log in'
-
     expect(page).to have_content('Signed in successfully')
     expect(page).to have_content('Sign Out')
   end
