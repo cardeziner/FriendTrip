f64466a27a9a5fcc2fa8fb75280e4452d9d8055f
Peter Stevens <cardeziner@gmail.com>
2021-03-02 18:25:32 -0700

    Scraper method syntax

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,5 +3,11 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  idaho_url = 'http://www.museumsusa.org/museums/?k=1271400%2cState%3aID%3bDirectoryID%3a200454'
 
+  html = open(idaho_url)
+
+  doc = Nokogiri::HTML(html)
+
+  binding.pry
 end
