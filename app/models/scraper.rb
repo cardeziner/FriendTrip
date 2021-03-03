092e80d0ca407a2ec9302eb9dbc891727a55014a
Peter Stevens <cardeziner@gmail.com>
2021-03-02 18:46:11 -0700

    Webscrape format

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,11 +3,16 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  idaho_url = 'http://www.museumsusa.org/museums/?k=1271400%2cState%3aID%3bDirectoryID%3a200454'
 
-  html = open(idaho_url)
+  def scrape_city_urls
+    idaho_url = 'http://www.museumsusa.org/museums/?k=1271400%2cState%3aID%3bDirectoryID%3a200454'
+    html = open(idaho_url)
+    doc = Nokogiri::HTML(html)
 
-  doc = Nokogiri::HTML(html)
+    cities = doc.css('#city').css('.browseCategoryItem').css('a')
+    binding.pry
+  end
 
-
+  scrape = Scraper.new
+  scrape.scrape_city_urls
 end
