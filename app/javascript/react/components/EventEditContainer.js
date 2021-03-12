e8d65005bf7dad7b8bf854400133caef8e065888
Peter Stevens <cardeziner@gmail.com>
2021-03-11 19:08:34 -0700

    event container setup

--- app/javascript/react/components/EventEditContainer.js
+++ app/javascript/react/components/EventEditContainer.js
@@ -5,9 +5,45 @@ const EventEditContainer = props =>{
  const [redirect, setRedirect] = useState(false)
  const [eventEditField, setEventEditField] = useState({})
 
- debugger 
+ const tripId = props.match.params.id
+
+ const editRestaurant = (editFormPayload) =>{
+    fetch(`/api/v1/trips/${tripId}`, {
+      credentials: "same-origin",
+      method: 'PATCH',
+      body: JSON.stringify(editFormPayload),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if(response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+        error = new Error(errorMessage);
+        throw(error)
+      }
+    })
+    .then(response => response.json())
+    .then(parsedTrip => {
+      debugger
+      setRedirect(true)
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  }
+
+  if (redirect) {
+    return <Redirect to={`/trips/${tripId}/events`} />
+  }
+
+
   return(
-    <div>hello world</div>
+    <div>
+    hello world
+    
+    </div>
   )
 }
 
