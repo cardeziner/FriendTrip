6226ea75d1f5aebe1bed02e330f71e0bf1323dd0
Peter Stevens <cardeziner@gmail.com>
2021-03-11 18:33:45 -0700

    edit container / routes

--- app/javascript/react/components/EventEditContainer.js
+++ app/javascript/react/components/EventEditContainer.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 
 const EventEditContainer = props =>{
+ const [redirect, setRedirect] = useState(false)
+ const [eventEditField, setEventEditField] = useState({})
 
+ debugger 
   return(
     <div>hello world</div>
   )
