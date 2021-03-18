ce805d91795dccf4e578531813cb5c9a5d8e3ae6
Peter Stevens <cardeziner@gmail.com>
2021-03-17 19:34:38 -0600

    tripflight model and migration file

--- app/javascript/react/components/TripShow.js
+++ app/javascript/react/components/TripShow.js
@@ -20,7 +20,7 @@ const TripShow = props =>{
   const [tripCity, setTripCity] = useState({})
   const [click, setClick] = useState(true)
   const [toggle, setToggle] = useState("hide")
-
+  debugger
   const iD = (props.id - 1)
 
   useEffect(() =>{
@@ -173,6 +173,7 @@ const TripShow = props =>{
       tally += cost
     }
   )
+  debugger
 
   return(
     <div className="bg" style={sectionStyle}>
@@ -197,6 +198,7 @@ const TripShow = props =>{
           <h3 className="text-white vert"><img src={dates} className="icon inline center"/><h5 className="center font inline">{date(props.trip.start_date)} - {date(props.trip.end_date)}</h5></h3>
             <h3 className="text-white vert"><img src={cost} className="icon inline center"/><h5 className="center  font inline">Your Costs: ${tally} </h5></h3><br/>
             <div className="flex vert"><img src={friends} className="inline icon fifty"/><div className="inline">{blankUser()}</div></div>
+            <div className="vert">hello</div>
               <div>
               <h5 className="font center accent-white" onClick={change}> + INVITE A FRIEND</h5>
                 <div id="form-info" className={toggle}>
