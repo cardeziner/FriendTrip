e9855cb5c0a80b91b747d6682e2302b00a020dcc
Peter Stevens <cardeziner@gmail.com>
2021-04-01 14:49:00 -0600

    am pm function

--- app/javascript/react/components/TripShow.js
+++ app/javascript/react/components/TripShow.js
@@ -106,16 +106,18 @@ const TripShow = props =>{
       }
     }
 
-  function formatAMPM(date) {
+  function formatAMPM(unparsedDate) {
+    date = new Date(unparsedDate)
+    var hours = date.getHours();
     debugger
-    var hours = date.prototype.getHours();
-    var minutes = date.prototype.getMinutes();
+    var minutes = date.getMinutes();
     var ampm = hours >= 12 ? 'pm' : 'am';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
-    return strTime;
+    return(strTime);
+
 }
 
     function date(date) {
