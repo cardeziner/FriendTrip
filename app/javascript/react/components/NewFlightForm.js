86050835bf5658ebbe2c6cca2a501a95fd167d4f
Peter Stevens <cardeziner@gmail.com>
2021-04-12 17:45:51 -0600

    conditional rendering debug

--- app/javascript/react/components/NewFlightForm.js
+++ app/javascript/react/components/NewFlightForm.js
@@ -1,136 +1,134 @@
 import React, {useState, useEffect} from 'react'
 
 const NewFlightForm = props =>{
-
-  function userCheck() {
-    if (props.userId) {
-      return(props.userId)
-    }
-  }
-
-
-  const [errors, setErrors] = useState({})
-  const [userId, setUserId] = useState({})
-  const [newFormPayload, setNewFormPayload] = useState({
-    airline: "",
-    on_time_status: "N/A",
-    departure_date: "",
-    departure_time: "",
-    arrival_date: "",
-    arrival_time: "",
-    user_id: userCheck(),
-    trip_id: props.tripId,
-  })
-
-  const handleInputChange = event =>{
-    setNewFormPayload({
-      ...newFormPayload,
-      [event.currentTarget.name]: event.currentTarget.value
+  if(props.userId){
+    const [errors, setErrors] = useState({})
+    const [userId, setUserId] = useState({})
+    const [newFormPayload, setNewFormPayload] = useState({
+      airline: "",
+      on_time_status: "N/A",
+      departure_date: "",
+      departure_time: "",
+      arrival_date: "",
+      arrival_time: "",
+      user_id: props.userId,
+      trip_id: props.tripId,
     })
-  }
 
-  const validForSubmission = () =>{
-    let submitErrors = {}
-    const requiredFields = ["airline", "on_time_status", "departure_date", "departure_time", "arrival_date",  "arrival_time"]
-    requiredFields.forEach(field =>{
-      if (newFormPayload[field].trim() === ""){
-        submitErrors = {
-          ...submitErrors,
-          [field]: "is blank"
-        }
-      }
-    })
-    setErrors(submitErrors)
-    return _.isEmpty(submitErrors)
-  }
-
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    if (validForSubmission()) {
-      props.addNewFlight({ flight: newFormPayload })
+    const handleInputChange = event =>{
       setNewFormPayload({
-        airline: "",
-        on_time_status: "",
-        departure_date: "",
-        departure_time: "",
-        arrival_date: "",
-        arrival_time: "",
-        user_id: props.userId,
-        trip_id: props.tripId,
+        ...newFormPayload,
+        [event.currentTarget.name]: event.currentTarget.value
       })
-      setErrors({})
     }
-  }
 
-  return(
-    <div className="center">
-    <h1 className="bold-blue">ENTER FLIGHT INFO </h1>
-      <form onSubmit={handleSubmit} className="wide-field">
-        <label className="airline text-white">
-          Airline<br/>
-          <input
-            name="airline"
-            id="airline"
-            type="airline"
-            onChange={handleInputChange}
-            value={newFormPayload.airline}
-          />
-        </label><br/>
-        <label className="on_time_status text-white">
-          On-time status<br/>
-          <input
-            name="on_time_status"
-            id="on_time_status"
-            type="on_time_status"
-            onChange={handleInputChange}
-            value={newFormPayload.on_time_status}
-          />
-        </label><br/>
-        <label className="name text-white">
-          departure_date<br/>
-          <input
-            name="departure_date"
-            id="departure_date"
-            type="date"
-            onChange={handleInputChange}
-            value={newFormPayload.departure_date}
-          />
-        </label><br/>
-        <label className="name text-white">
-          departure_time<br/>
-          <input
-            name="departure_time"
-            id="departure_time"
-            type="time"
-            onChange={handleInputChange}
-            value={newFormPayload.departure_time}
-          />
-        </label><br/>
-        <label className="name text-white">
-          arrival_date<br/>
-          <input
-            name="arrival_date"
-            id="arrival_date"
-            type="date"
-            onChange={handleInputChange}
-            value={newFormPayload.arrival_date}
-          />
-        </label><br/>
-        <label className="name text-white">
-          arrival_time<br/>
-          <input
-            name="arrival_time"
-            id="arrival_time"
-            type="time"
-            onChange={handleInputChange}
-            value={newFormPayload.arrival_time}
-          />
-        </label><br/><br/>
-        <input className="btn btn-primary text center" type="submit" value="Add Flight" />
-      </form><br/>
-    </div>
-  )
+    const validForSubmission = () =>{
+      let submitErrors = {}
+      const requiredFields = ["airline", "on_time_status", "departure_date", "departure_time", "arrival_date",  "arrival_time"]
+      requiredFields.forEach(field =>{
+        if (newFormPayload[field].trim() === ""){
+          submitErrors = {
+            ...submitErrors,
+            [field]: "is blank"
+          }
+        }
+      })
+      setErrors(submitErrors)
+      return _.isEmpty(submitErrors)
+    }
+
+
+    const handleSubmit = event => {
+      event.preventDefault()
+      if (validForSubmission()) {
+        props.addNewFlight({ flight: newFormPayload })
+        setNewFormPayload({
+          airline: "",
+          on_time_status: "",
+          departure_date: "",
+          departure_time: "",
+          arrival_date: "",
+          arrival_time: "",
+          user_id: props.userId,
+          trip_id: props.tripId,
+        })
+        setErrors({})
+      }
+    }
+
+    return(
+      <div className="center">
+      <h1 className="bold-blue">ENTER FLIGHT INFO </h1>
+        <form onSubmit={handleSubmit} className="wide-field">
+          <label className="airline text-white">
+            Airline<br/>
+            <input
+              name="airline"
+              id="airline"
+              type="airline"
+              onChange={handleInputChange}
+              value={newFormPayload.airline}
+            />
+          </label><br/>
+          <label className="on_time_status text-white">
+            On-time status<br/>
+            <input
+              name="on_time_status"
+              id="on_time_status"
+              type="on_time_status"
+              onChange={handleInputChange}
+              value={newFormPayload.on_time_status}
+            />
+          </label><br/>
+          <label className="name text-white">
+            departure_date<br/>
+            <input
+              name="departure_date"
+              id="departure_date"
+              type="date"
+              onChange={handleInputChange}
+              value={newFormPayload.departure_date}
+            />
+          </label><br/>
+          <label className="name text-white">
+            departure_time<br/>
+            <input
+              name="departure_time"
+              id="departure_time"
+              type="time"
+              onChange={handleInputChange}
+              value={newFormPayload.departure_time}
+            />
+          </label><br/>
+          <label className="name text-white">
+            arrival_date<br/>
+            <input
+              name="arrival_date"
+              id="arrival_date"
+              type="date"
+              onChange={handleInputChange}
+              value={newFormPayload.arrival_date}
+            />
+          </label><br/>
+          <label className="name text-white">
+            arrival_time<br/>
+            <input
+              name="arrival_time"
+              id="arrival_time"
+              type="time"
+              onChange={handleInputChange}
+              value={newFormPayload.arrival_time}
+            />
+          </label><br/><br/>
+          <input className="btn btn-primary text center" type="submit" value="Add Flight" />
+        </form><br/>
+      </div>
+    )
+  }else{
+    return(
+      <div>Loading...Please Wait</div>
+    )
+  }
 }
 
 export default NewFlightForm
