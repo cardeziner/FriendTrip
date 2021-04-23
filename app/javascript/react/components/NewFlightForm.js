086d880f881b6950ca1ebf3bfa66acf1613b900a
Peter Stevens <cardeziner@gmail.com>
2021-04-23 14:32:39 -0600

    form styling

--- app/javascript/react/components/NewFlightForm.js
+++ app/javascript/react/components/NewFlightForm.js
@@ -82,67 +82,17 @@ const NewFlightForm = props =>{
       <h1 className="bold-blue">ENTER FLIGHT INFO </h1>
         <form onSubmit={handleSubmit} className="wide-field">
           <label className="airline text-white">
-            Airline<br/>
+            AIRLINE<br/>
             <input
               name="airline"
               id="airline"
-              type="airline"
+              type="text"
               onChange={handleInputChange}
               value={newFormPayload.airline}
             />
           </label><br/>
-          <label className="on_time_status text-white">
-            Check if flights status is on-time<br/>
-            <input
-              name="on_time_status"
-              id="on_time_status"
-              type="checkbox"
-              onChange={handleInputChange}
-              value={newFormPayload.on_time_status}
-            />
-          </label><br/>
           <label className="name text-white">
-            departure_date<br/>
-            <input
-              name="departure_date"
-              id="departure_date"
-              type="date"
-              onChange={handleInputChange}
-              value={newFormPayload.departure_date}
-            />
-          </label><br/>
-          <label className="name text-white">
-            departure_time<br/>
-            <input
-              name="departure_time"
-              id="departure_time"
-              type="time"
-              onChange={handleInputChange}
-              value={newFormPayload.departure_time}
-            />
-          </label><br/>
-          <label className="name text-white">
-            arrival_date<br/>
-            <input
-              name="arrival_date"
-              id="arrival_date"
-              type="date"
-              onChange={handleInputChange}
-              value={newFormPayload.arrival_date}
-            />
-          </label><br/>
-          <label className="name text-white">
-            arrival_time<br/>
-            <input
-              name="arrival_time"
-              id="arrival_time"
-              type="time"
-              onChange={handleInputChange}
-              value={newFormPayload.arrival_time}
-            />
-          </label><br/>
-          <label className="name text-white">
-            departing_airport<br/>
+            DEPARTING AIRPORT<br/>
             <input
               name="departing_airport"
               id="departing_airport"
@@ -152,7 +102,7 @@ const NewFlightForm = props =>{
             />
           </label><br/>
           <label className="name text-white">
-            arriving_airport<br/>
+            ARRIVING AIRPORT<br/>
             <input
               name="arriving_airport"
               id="arriving_airport"
@@ -160,7 +110,57 @@ const NewFlightForm = props =>{
               onChange={handleInputChange}
               value={newFormPayload.arriving_airport}
             />
-          </label>
+          </label><br/>
+          <label className="on_time_status text-white">
+            IS FLIGHT STATUS 'ON TIME'?<br/>
+            <input
+              name="on_time_status"
+              id="on_time_status"
+              type="checkbox"
+              onChange={handleInputChange}
+              value={newFormPayload.on_time_status}
+            />
+          </label><br/>
+          <label className="name text-white">
+            DEPARTURE DATE<br/>
+            <input
+              name="departure_date"
+              id="departure_date"
+              type="date"
+              onChange={handleInputChange}
+              value={newFormPayload.departure_date}
+            />
+          </label><br/>
+          <label className="name text-white">
+            DEPARTURE TIME<br/>
+            <input
+              name="departure_time"
+              id="departure_time"
+              type="time"
+              onChange={handleInputChange}
+              value={newFormPayload.departure_time}
+            />
+          </label><br/>
+          <label className="name text-white">
+            ARRIVAL DATE<br/>
+            <input
+              name="arrival_date"
+              id="arrival_date"
+              type="date"
+              onChange={handleInputChange}
+              value={newFormPayload.arrival_date}
+            />
+          </label><br/>
+          <label className="name text-white">
+            ARRIVAL TIME<br/>
+            <input
+              name="arrival_time"
+              id="arrival_time"
+              type="time"
+              onChange={handleInputChange}
+              value={newFormPayload.arrival_time}
+            />
+          </label><br/>
           <br/><br/>
           <input className="btn btn-primary text center" type="submit" value="Add Flight" />
         </form><br/>
