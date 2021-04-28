83e062e6a61027b379bb50172ff703ddfacef352
Peter Stevens <cardeziner@gmail.com>
2021-04-27 21:35:42 -0600

    on-time API

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -14,21 +14,23 @@ end
 
 def flight_data
 
-url = URI("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/referral/v1.0/%7Bcountry%7D/%7Bcurrency%7D/%7Blocale%7D/%7Boriginplace%7D/%7Bdestinationplace%7D/%7Boutboundpartialdate%7D/%7Binboundpartialdate%7D?shortapikey=ra66933236979928&apiKey=%7Bshortapikey%7D")
+  url = URI("https://airport-on-time-performance.p.rapidapi.com/airport/predictions/on-time?airportCode=BOS&date=2021-04-29")
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-request = Net::HTTP::Get.new(url)
-request["x-rapidapi-key"] = '26169f8158msh2412dd030a7ba8ep1feac3jsn87364f9e3c07'
-request["x-rapidapi-host"] = 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com'
+  request = Net::HTTP::Get.new(url)
+  request["x-rapidapi-key"] = '26169f8158msh2412dd030a7ba8ep1feac3jsn87364f9e3c07'
+  request["x-rapidapi-host"] = 'airport-on-time-performance.p.rapidapi.com'
 
-response = http.request(request)
-puts response.read_body
+  response = http.request(request)
+  
+  puts response.read_body
 end
 
 def create
+
   flight = Flight.create(strong_params)
   tripflight = Tripflight.create(trip_id: trip_params["trip_id"], flight_id: flight.id)
   userflight = Userflight.create(user_id: user_params["user_id"], flight_id: flight.id)
