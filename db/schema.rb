dfe871c60735de9e1a29b69ca25f27bb4c8baa53
Peter Stevens <cardeziner@gmail.com>
2021-05-06 22:35:05 -0600

    schema cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -122,15 +122,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_040022) do
     t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer "invitation_limit"
-    t.integer "invited_by_id"
-    t.string "invited_by_type"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
