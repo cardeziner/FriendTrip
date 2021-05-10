7d7e58252b1327ff34ca27fe8417d741457e105d
Peter Stevens <cardeziner@gmail.com>
2021-05-09 22:03:23 -0600

    Places autocomplete

--- app/javascript/react/components/GooglePlaceComponent.js
+++ app/javascript/react/components/GooglePlaceComponent.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import SearchBar from 'material-ui-search-bar'
+import Script from 'react-load-script'
 
 const GooglePlaceComponent = (props) =>{
+  const [parameters, setParameters] = {
+    city: 'Denver',
+    query: 'Crawford Hotel'
+  }
+
+
+
   return(
-    <div>HELLO FROM GOOGLE PLACE COMPONENT</div>
+    <div>
+      <SearchBar
+      id="autocomplete"
+      placeholder=""
+      hintText={parameters.city}
+      value={parameters.query}
+      className="search"
+      />
+    </div>
   )
 }
+
 export default GooglePlaceComponent
