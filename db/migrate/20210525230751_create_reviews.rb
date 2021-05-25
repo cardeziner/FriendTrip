c332518af7b89565645c3d91f1bac1c78fa95971
Peter Stevens <cardeziner@gmail.com>
2021-05-25 17:26:14 -0600

    updated schema

--- db/migrate/20210525230751_create_reviews.rb
+++ db/migrate/20210525230751_create_reviews.rb
@@ -3,6 +3,9 @@ class CreateReviews < ActiveRecord::Migration[5.2]
     create_table :reviews do |t|
       t.integer :rating, null: false
       t.string :review, null: false
+      t.belongs_to :user, null: false
+
+      t.timestamps null: false
     end
   end
 end
