721b22146b48a5700b1148b09f68fe6436d0185c
Peter Stevens <cardeziner@gmail.com>
2021-05-30 21:29:32 -0600

    add new review function

--- app/javascript/react/components/NewReviewForm.js
+++ app/javascript/react/components/NewReviewForm.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const NewReviewForm = props =>{
+  const [review, setReview] = useState({})
+  const [newFormPayload, setNewFormPayload] = useState({
+    rating: 0,
+    review: "",
+  })
+
+  const addNewReview = (formPayload) => {
+    fetch('/api/v1/reviews', {
+        credentials: "same-origin",
+        method: 'POST',
+        body: JSON.stringify(formPayload),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        if(response.ok) {
+          return response
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+          throw(error)
+        }
+      })
+      .then(response => response.json())
+      .then(parsedNewReview => {
+        setReview(parsedNewReview)
+      })
+      .catch(error => console.error(`Error in fetch: ${errorMessage}`))
+    }
+
+
+
   return(
     <div>hello world</div>
   )
