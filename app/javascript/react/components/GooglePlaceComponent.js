eb61d7ca833f7ff9ea4a489f1056bfe2467ec28e
Peter Stevens <cardeziner@gmail.com>
2021-06-30 20:43:35 -0600

    hotel autocomplete debug/refactor

--- app/javascript/react/components/GooglePlaceComponent.js
+++ app/javascript/react/components/GooglePlaceComponent.js
@@ -1,75 +1,69 @@
 import React, { useState } from 'react'
 import SearchBar from 'material-ui-search-bar'
 import Script from 'react-load-script'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 const GooglePlaceComponent = (props) =>{
-  const [results, setResults] = useState({})
-  const [place, setPlace] = useState({
-      name: '',
-      city: '',
-      query: ''
-  })
-
-  // see if you can define `autocomplete` outside of handleScriptLoad
-
-  let autocomplete
+  const [value, setValue] = useState(null);
 
 
-  const handleScriptLoad = () =>{
-    const options = {
-      types: ['establishment'],
-    }
+  // const [results, setResults] = useState({})
+  // const [place, setPlace] = useState({
+  //     name: '',
+  //     city: '',
+  //     query: ''
+  // })
+  //
+  // // see if you can define `autocomplete` outside of handleScriptLoad
+  //
+  // let autocomplete
+  //
+  // const handleScriptLoad = () =>{
+  //   const options = {
+  //     types: ['establishment'],
+  //   }
+  //
+  //   autocomplete = new google.maps.places.Autocomplete(
+  //     document.getElementById('autocomplete'),
+  //     options,
+  //   )
+  //
+  //   autocomplete.setFields(['address_components', 'formatted_address'])
+  //   autocomplete.addListener('place_changed', handlePlaceSelect())
+  //   // this might work but try the
+  // }
+  //
+  // const handlePlaceSelect = () =>{
+  //   debugger
+  //   const addressObject = autocomplete.getPlace();
+  //   const address = addressObject.address_components;
+  //   if (address) {
+  //     debugger
+  //     setResults(address)
+  //     setPlace(
+  //       {
+  //         name: address.gm_accessors_.place.We.formattedPrediction.split(",")[0],
+  //         city: address.address[0].long_name,
+  //         query: address.formatted_address,
+  //       }
+  //     )
+  //   }
+  // }
 
-    autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('autocomplete'),
-      options,
-    )
-
-    autocomplete.setFields(['address_components', 'formatted_address'])
-    autocomplete.addListener('place_changed', handlePlaceSelect())
-    // this might work but try the
-  }
-
-  const handlePlaceSelect = () =>{
-    debugger
-    const addressObject = autocomplete.getPlace();
-    const address = addressObject.address_components;
-    if (address) {
-      debugger
-      setResults(address)
-      setPlace(
-        {
-          name: address.gm_accessors_.place.We.formattedPrediction.split(",")[0],
-          city: address.address[0].long_name,
-          query: address.formatted_address,
-        }
-      )
-    }
-  }
-
-
+  //
 
   const googleAPI = process.env.REACT_APP_GOOGLE_API_KEY
 
-  const scriptURL = (`https://maps.googleapis.com/maps/api/js?key=${googleAPI}&libraries=places`).to_s
-
     return (
       <div>
-        <Script
-          url={scriptURL}
-          onLoad={handleScriptLoad}
-        />
-        <SearchBar
-          id="autocomplete"
-          placeholder=""
-          hintText="Search Establishment"
-          onChange={handleScriptLoad}
-          style={{
-            margin: '0 auto',
-            maxWidth: 800,
+        <GooglePlacesAutocomplete
+
+          onLoadFailed={(error) => (console.error("Could not inject Google script", error))}
+          selectProps={{
+            value,
+            onChange: setValue,
           }}
         />
-        <h1>hello{place.name}</h1>
       </div>
     );
 }
