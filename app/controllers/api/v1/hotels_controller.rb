e5791fb09ee01542a5789982dfe27c224b3e4aae
Peter Stevens <cardeziner@gmail.com>
2021-07-01 18:14:43 -0600

    hotel create action

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -7,7 +7,13 @@ class Api::V1::HotelsController < ApplicationController
   end
 
   def create
-    hotel = Hotel.new(hotel_params)
+    @user = current_user
+    @hotel = Hotel.new(hotel_params, user: @user)
+    if @hotel.save
+      render json: hotel.errors.full_messages, status: :unprocessable_entity
+    else
+      render json: @review.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
 private
@@ -16,4 +22,8 @@ private
     params.require(:hotel).permit(:name, :address, :city, :state, :check_in, :check_out)
   end
 
+  def trip_params
+    params.require(:hotel.permit(:trip_id))
+  end
+  
 end
