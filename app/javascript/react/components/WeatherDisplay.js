0f327bef156b99a67eb20841d2368ae402d34591
Peter Stevens <cardeziner@gmail.com>
2021-08-25 15:56:10 -0600

    conditional weather display

--- app/javascript/react/components/WeatherDisplay.js
+++ app/javascript/react/components/WeatherDisplay.js
@@ -1,8 +1,15 @@
 import React, {useState, useEffect} from 'react'
+import cloudy from '../../../assets/images/cloudy.png'
+import clear from '../../../assets/images/clear.png'
+import partly from '../../../assets/images/partly-cloudy.png'
+import rain from '../../../assets/images/rain.png'
+import sleet from '../../../assets/images/sleet.png'
+import snow from '../../../assets/images/snow.png'
 
 const WeatherDisplay = (props) =>{
 
   if( 0 > props.longitude > 0){
+    const [icon, setIcon] = useState("")
     const [currentWeather, setCurrentWeather] = useState({})
     const [dailyWeather, setDailyWeather] = useState({})
     const [forecastData, setForecastData] = useState([])
@@ -29,6 +36,7 @@ const WeatherDisplay = (props) =>{
           setCurrentWeather(parsedWeatherData.currently)
           setDailyWeather(parsedWeatherData.daily)
           setForecastData(parsedWeatherData.daily.data)
+
         })
         .catch(error => console.error(`Error in fetch ${errorMessage}`))
       }, [])
@@ -49,11 +57,39 @@ const WeatherDisplay = (props) =>{
         )
       })
 
+      let imageURL = ''
+
+
+      if(dailyWeather.icon){
+        if(status.includes("clear")){
+          imageURL = '../../../assets/images/clear.png'
+        }
+        if(status.includes("rain")){
+          imageURL = '../../../assets/images/rain.png'
+        }
+        if(status.includes("partly")){
+          imageURL = '../../../assets/images/partly-cloudy.png'
+        }
+        if(status.includes("sleet")){
+          imageURL = '../../../assets/images/sleet.png'
+        }
+        if(status.includes("snow")){
+          imageURL = '../../../assets/images/snow.png'
+        }
+        if(status.includes("cloudy")){
+          imageURL = '../../../assets/images/cloudy.png'
+        }
+        debugger
+      }else{
+        imageURL = sleet
+      }
+
       // partly cloudy, mostly cloudy, clear, possible light rain, humid and overcast, clear, possible light rain and humid, overcast,
 // clear-day, rain, partly-cloudy day, sleet, snow, cloudy
 
     return(
       <div>
+        <img src={imageURL}/>
         <h5 className="text-white center">CURRENT TEMP</h5>
         <h1 className="text-white center">
 
