c0f63eac540987c3ba262f38585f3b98edd5c112
Peter Stevens <cardeziner@gmail.com>
2021-09-18 17:24:22 -0600

    remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+# #
+# # trip1 = Trip.create(name:"Boys Trip", city:"San Diego", state:"CA", start_date: "09/15/2020", end_date: "09/20/2020")
+# # trip2 = Trip.create(name:"Family Fun", city:"Jacksonville", state:"FL", start_date: "07/11/2020", end_date: "07/17/2020")
+# # trip3 =Trip.create(name:"Work Trip", city:"San Diego", state:"CA", start_date: "10/20/2020", end_date: "10/23/2020")
 #
-# Examples:
+# user1 = User.create(first_name: "Peter", last_name: "Stevens", email:"peter@gmail.com", password: "password")
+# user2 = User.create(first_name: "Jeffrey", last_name: "Jefferson", email:"jeffy@gmail.com", password: "password")
+# # user3 = User.create(first_name: "Billiam", last_name: "Cornwall", email:"Iambill@gmail.com", password: "password")
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# trip1 = Trip.create(id: 1, name:"Boys Trip", city:"San Diego", state:"CA", start_date: "09/15/2020", end_date: "09/20/2020")
+# trip1 = Trip.create(id: 2, name:"Peters Trip", city:"San Diego", state:"CA", start_date: "09/15/2020", end_date: "09/20/2020")
 #
-# trip1 = Trip.create(name:"Boys Trip", city:"San Diego", state:"CA", start_date: "09/15/2020", end_date: "09/20/2020")
-# trip2 = Trip.create(name:"Family Fun", city:"Jacksonville", state:"FL", start_date: "07/11/2020", end_date: "07/17/2020")
-# trip3 =Trip.create(name:"Work Trip", city:"San Diego", state:"CA", start_date: "10/20/2020", end_date: "10/23/2020")
-
-user1 = User.create(first_name: "Peter", last_name: "Stevens", email:"peter@gmail.com", password: "password")
-user2 = User.create(first_name: "Jeffrey", last_name: "Jefferson", email:"jeffy@gmail.com", password: "password")
-# user3 = User.create(first_name: "Billiam", last_name: "Cornwall", email:"Iambill@gmail.com", password: "password")
-
-trip1 = Trip.create(id: 1, name:"Boys Trip", city:"San Diego", state:"CA", start_date: "09/15/2020", end_date: "09/20/2020")
-trip1 = Trip.create(id: 2, name:"Peters Trip", city:"San Diego", state:"CA", start_date: "09/15/2020", end_date: "09/20/2020")
-
-
-tripmember1 = Tripmember.create(user_id: 1, trip_id: 1)
-tripmember2 = Tripmember.create(user_id: 2, trip_id: 1)
-tripmember1 = Tripmember.create(user_id: 1, trip_id: 2)
-
-
-# flight1 = Flight.create(airline: "United", on_time_status: "yes", departure_date: "11/01/2021", departure_time: "06:10:17", arrival_date: "11/01/2021", arrival_time: "09:10:17")
-# flight2 = Flight.create(airline: "American", on_time_status: "yes", departure_date: "11/02/2021", departure_time: "06:10:17", arrival_date: "11/03/2021", arrival_time: "16:10:17")
-# flight3 = Flight.create(airline: "Spirit", on_time_status: "yes", departure_date: "11/02/2021", departure_time: "06:10:17", arrival_date: "11/02/2021", arrival_time: "09:10:17")
 #
-# userflight1 = Userflight.create(user_id: 1, flight_id: 1)
+# tripmember1 = Tripmember.create(user_id: 1, trip_id: 1)
+# tripmember2 = Tripmember.create(user_id: 2, trip_id: 1)
+# tripmember1 = Tripmember.create(user_id: 1, trip_id: 2)
 #
-# tripflight = Tripflight.create(trip_id: 1, flight_id: 1)
-# tripflight1 = Tripflight.create(trip_id: 1, flight_id: 2)
 #
-# tripflight2 = Tripflight.create(trip_id: 2, flight_id: 3)
-
-# hotel1 = Hotel.create(name: "Crawford Hotel", address: "2400 Wazee Street", city: "Denver", state: "CO", check_in: "2021-05-04 21:45:23", check_out: "2021-05-04 21:45:24")
+# # flight1 = Flight.create(airline: "United", on_time_status: "yes", departure_date: "11/01/2021", departure_time: "06:10:17", arrival_date: "11/01/2021", arrival_time: "09:10:17")
+# # flight2 = Flight.create(airline: "American", on_time_status: "yes", departure_date: "11/02/2021", departure_time: "06:10:17", arrival_date: "11/03/2021", arrival_time: "16:10:17")
+# # flight3 = Flight.create(airline: "Spirit", on_time_status: "yes", departure_date: "11/02/2021", departure_time: "06:10:17", arrival_date: "11/02/2021", arrival_time: "09:10:17")
+# #
+# # userflight1 = Userflight.create(user_id: 1, flight_id: 1)
+# #
+# # tripflight = Tripflight.create(trip_id: 1, flight_id: 1)
+# # tripflight1 = Tripflight.create(trip_id: 1, flight_id: 2)
+# #
+# # tripflight2 = Tripflight.create(trip_id: 2, flight_id: 3)
+#
+# # hotel1 = Hotel.create(name: "Crawford Hotel", address: "2400 Wazee Street", city: "Denver", state: "CO", check_in: "2021-05-04 21:45:23", check_out: "2021-05-04 21:45:24")
