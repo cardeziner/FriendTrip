3ee6284aadeb726605af5b55bf90d08671bf5756
Peter Stevens <cardeziner@gmail.com>
2023-05-01 16:19:36 -0600

    add proxy

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "friendtrip-bt.herokuapp.com" }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
